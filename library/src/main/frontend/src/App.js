b4b8166a85baa6a1c47890923e235752e78148ef
jaeho13 <jaehooo13@naver.com>
2024-06-27 23:55:59 +0900

    "0627 jaeho 관리자 페이지 이동"

--- library/src/main/frontend/src/App.js
+++ library/src/main/frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />} />
+                    <Route path="/admin" />
                 </Routes>
             </BrowserRouter>
         </>
