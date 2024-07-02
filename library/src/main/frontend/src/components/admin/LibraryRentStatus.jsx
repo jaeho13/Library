1e97b120013748a22be42609854894346e0f3953
jaeho13 <jaehooo13@naver.com>
2024-07-03 00:08:52 +0900

    "0703 jaeho 도서 대여 현황 리네임 및 디자인"

---
+++ library/src/main/frontend/src/components/admin/LibraryRentStatus.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+
+const LibraryRentStatus = () => {
+    return (
+        <>
+            <TopMenu>
+                <PageName>
+                    도서 대여 현황
+                </PageName>
+            </TopMenu>
+
+
+            <LeftMenu>
+                <LeftRentStatus>
+
+                </LeftRentStatus>
+
+                <LeftBookStatus>
+
+                </LeftBookStatus>
+
+                <LeftUserStatus>
+
+                </LeftUserStatus>
+            </LeftMenu>
+        </>
+    )
+}
+
+export default LibraryRentStatus;
+
+const TopMenu = styled.div`
+    width: 100%;
+    height: 20vh;
+    border: 2px solid blue;
+    display: flex;
+    align-items: center;
+`
+
+const PageName = styled.div`
+    width: 30%;
+    height: 15vh;
+    border: 2px solid red;
+    margin-left: 10%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    color: white;
+    font-size: 1.5rem;
+`
+
+const LeftMenu = styled.div`
+    width: 10%;
+    height: 80vh;
+    border: 2px solid red;
+    display: flex;
+    flex-direction: column;
+`
+
+const LeftRentStatus = styled.div`
+    width: 100%;
+    height: 10vh;
+    border: 2px solid blue;
+`
+
+const LeftBookStatus = styled.div`
+    width: 100%;
+    height: 10vh;
+    border: 2px solid black;
+`
+
+const LeftUserStatus = styled.div`
+    width: 100%;
+    height: 10vh;
+    border: 2px solid green;
+`
