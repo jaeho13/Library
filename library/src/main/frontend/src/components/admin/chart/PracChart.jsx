d93351995451891714cf9aa305bb46d35a1d8d7c
jaeho13 <jaehooo13@naver.com>
2024-07-05 01:35:35 +0900

    "0705 jaeho 차트 디자인 연습"

--- library/src/main/frontend/src/components/admin/chart/PracChart.jsx
+++ library/src/main/frontend/src/components/admin/chart/PracChart.jsx
@@ -5,67 +5,53 @@ import {
     CartesianGrid, Tooltip,
     Legend, ResponsiveContainer
 } from 'recharts';
+import styled from 'styled-components';
 
 const data = [
     {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        name: "부서 1",
+        num: 5
     },
     {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        name: "부서 2",
+        num: 3
     },
     {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        name: "부서 3",
+        num: 1
     },
     {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        name: "부서 4",
+        num: 2
+    }
+    ,
+    {
+        name: "부서 5",
+        num: 4
     },
     {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
+        name: "부서 6",
+        num: 2
+    }
 ];
-
 const PracChart = () => {
     return (
-        <div style={{ width: '100%', height: 400 }}>
-            <ResponsiveContainer>
+        <BarChartSize>
+            <ResponsiveContainer width="100%" height="100%">
                 <BarChart data={data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
+                    <Bar dataKey="num" fill="#8884d8" />
+                    <XAxis dataKey="name" tick={{ fontSize: 12 }} />
                     <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="uv" fill="#8884d8" />
                 </BarChart>
             </ResponsiveContainer>
-        </div>
+        </BarChartSize>
     );
 }
 
 export default PracChart;
+
+const BarChartSize = styled.div`
+    width: 100%;
+    /* height: 30vh; */
+    border: 2px solid red;
+`
