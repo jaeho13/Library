ed93d471ec19fb120e807bb6d060c3185c10bfe3
jaeho13 <jaehooo13@naver.com>
2024-07-07 23:51:38 +0900

    "0707 jaeho 코드 정리"

--- library/src/main/frontend/src/components/admin/chart/BookStatusPieChart.jsx
+++ library/src/main/frontend/src/components/admin/chart/BookStatusPieChart.jsx
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import styled from 'styled-components';
 
 
