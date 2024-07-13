f07c7cc467869b4b20077d588dda67020ea06813
jaeho13 <jaehooo13@naver.com>
2024-07-13 23:25:23 +0900

    "0713 jaeho props 필요 없는 디자인 삭제"

--- library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
@@ -26,27 +26,6 @@ const LibraryBookStatus = () => {
 
 export default LibraryBookStatus;
 
-const TopMenu = styled.div`
-    width: 100%;
-    height: 20vh;
-    border: 2px solid blue;
-    display: flex;
-    align-items: center;
-`
-
-const PageName = styled.div`
-    width: 30%;
-    height: 15vh;
-    border: 2px solid red;
-    margin-left: 10%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
-    color: white;
-    font-size: 1.5rem;
-`
-
 const LeftMenu = styled.div`
     width: 10%;
     height: 80vh;
--- library/src/main/frontend/src/components/admin/LibraryRentStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryRentStatus.jsx
@@ -49,27 +49,6 @@ const LibraryRentStatus = () => {
 
 export default LibraryRentStatus;
 
-const TopMenu = styled.div`
-    width: 100%;
-    height: 20vh;
-    border: 2px solid blue;
-    display: flex;
-    align-items: center;
-`
-
-const PageName = styled.div`
-    width: 30%;
-    height: 15vh;
-    border: 2px solid red;
-    margin-left: 10%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
-    color: white;
-    font-size: 1.5rem;
-`
-
 const Main = styled.div`
     border: 2px solid black;
     display: flex;
