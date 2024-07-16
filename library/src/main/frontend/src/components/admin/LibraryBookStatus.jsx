7c11694c9c30bfc352420973c75e40a12514b3a1
jaeho13 <jaehooo13@naver.com>
2024-07-17 01:39:38 +0900

    "0717 jaeho 도서 관리 페이지 기초 디자인"

--- library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
@@ -1,34 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 import TopSide from "../side/Topside";
+import { useNavigate } from "react-router-dom";
 
 const LibraryBookStatus = () => {
+
+    const navigate = useNavigate();
+
+    const rentPage = () => {
+        navigate("/admin/rent")
+    }
+
+    const userPage = () => {
+        navigate("/admin/user")
+    }
+
     return (
         <>
             <TopSide name="도서 관리" />
 
-            <LeftMenu>
-                <LeftRentStatus>
+            <Main>
+                <LeftMenu>
+                    <LeftRentStatus onClick={rentPage}>도서 대여 현황</LeftRentStatus>
+                    <LeftRentStatus>도서 관리</LeftRentStatus>
+                    <LeftRentStatus onClick={userPage}>회원 관리</LeftRentStatus>
+                </LeftMenu>
 
-                </LeftRentStatus>
+                <BookInfoBind>
+                    <BookManage>
+                        <BookAdd>
+                            도서 추가
+                        </BookAdd>
+                        <BookRemove>
+                            도서 삭제
+                        </BookRemove>
+                    </BookManage>
 
-                <LeftBookStatus>
+                    <BookInfoList>
 
-                </LeftBookStatus>
-
-                <LeftUserStatus>
-
-                </LeftUserStatus>
-            </LeftMenu>
+                    </BookInfoList>
+                </BookInfoBind>
+            </Main>
         </>
     )
 }
 
 export default LibraryBookStatus;
 
+const Main = styled.div`
+    border: 2px solid black;
+    display: flex;
+    flex-direction: row;
+`
+
 const LeftMenu = styled.div`
     width: 10%;
-    height: 80vh;
+    height: 60vh;
     border: 2px solid red;
     display: flex;
     flex-direction: column;
@@ -38,16 +65,49 @@ const LeftRentStatus = styled.div`
     width: 100%;
     height: 10vh;
     border: 2px solid blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `
 
-const LeftBookStatus = styled.div`
+const BookInfoBind = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
+
+const BookManage = styled.div`
     width: 100%;
     height: 10vh;
+    display: flex;
+    flex-direction: row;
     border: 2px solid black;
+    align-items: center;
 `
 
-const LeftUserStatus = styled.div`
+const BookAdd = styled.div`
+    width: 10%;
+    height: 5vh;
+    border: 2px solid blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 2%;
+`
+
+const BookRemove = styled.div`
+    width: 10%;
+    height: 5vh;
+    border: 2px solid blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 2%;
+`
+
+const BookInfoList = styled.div`
     width: 100%;
-    height: 10vh;
-    border: 2px solid green;
+    height: 3vh;
+    border: 2px solid red;
 `
