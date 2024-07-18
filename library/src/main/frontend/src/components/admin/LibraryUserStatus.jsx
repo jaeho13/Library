89fdbd5ee8c3a48ce49aee2db39cab9f23861bff
jaeho13 <jaehooo13@naver.com>
2024-07-18 22:49:57 +0900

    "0718 jaeho 회원관리 페이지 디자인"

--- library/src/main/frontend/src/components/admin/LibraryUserStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryUserStatus.jsx
@@ -1,35 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 import TopSide from "../side/Topside";
+import { useNavigate } from "react-router-dom";
 
 const LibraryUserStatus = () => {
+
+    const navigate = useNavigate();
+
+    const rentPage = () => {
+        navigate("/admin/rent")
+    }
+
+    const bookPage = () => {
+        navigate("/admin/book")
+    }
+
     return (
         <>
             <TopSide name="회원 관리" />
 
+            <Main>
 
-            <LeftMenu>
-                <LeftRentStatus>
-
-                </LeftRentStatus>
-
-                <LeftBookStatus>
-
-                </LeftBookStatus>
-
-                <LeftUserStatus>
-
-                </LeftUserStatus>
-            </LeftMenu>
+                <LeftMenu>
+                    <LeftRentStatus onClick={rentPage}>도서 대여 현황</LeftRentStatus>
+                    <LeftRentStatus onClick={bookPage}>도서 관리</LeftRentStatus>
+                    <LeftRentStatus>회원 관리</LeftRentStatus>
+                </LeftMenu>
+            </Main>
         </>
     )
 }
 
 export default LibraryUserStatus;
 
+
+const Main = styled.div`
+    border: 2px solid black;
+    display: flex;
+    flex-direction: row;
+`
+
 const LeftMenu = styled.div`
     width: 10%;
-    height: 80vh;
+    height: 60vh;
     border: 2px solid red;
     display: flex;
     flex-direction: column;
@@ -39,16 +52,8 @@ const LeftRentStatus = styled.div`
     width: 100%;
     height: 10vh;
     border: 2px solid blue;
-`
-
-const LeftBookStatus = styled.div`
-    width: 100%;
-    height: 10vh;
-    border: 2px solid black;
-`
-
-const LeftUserStatus = styled.div`
-    width: 100%;
-    height: 10vh;
-    border: 2px solid green;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `
