05f9ac189b3fb1b71ecffb69c7a23598a5c529f4
jaeho13 <jaehooo13@naver.com>
2024-07-24 01:22:46 +0900

    "0724 jaeho 도서 관리 페이지 도서 리스트 받아오기"

--- library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import TopSide from "../side/TopSide";
 import LeftSide from "../side/LeftSide";
+import axios from "axios";
 
 const LibraryBookStatus = () => {
+
+    const [bookList, setBookList] = useState([]);
+
+    useEffect(() => {
+        const bookListLoad = async () => {
+            try {
+                const response = await axios.get("/book/findBookList");
+                setBookList(response.data.bookList);
+                console.log("책 리스트 데이터 들어왔다");
+            } catch (error) {
+                console.log("책 리스트 데이터 안 들어왔다");
+            }
+        };
+        bookListLoad();
+    }, []);
 
     return (
         <>
@@ -15,17 +31,31 @@ const LibraryBookStatus = () => {
 
                 <BookInfoBind>
                     <BookManage>
-                        <BookAdd>
-                            도서 추가
-                        </BookAdd>
-                        <BookRemove>
-                            도서 삭제
-                        </BookRemove>
+                        <BookAdd>도서 추가</BookAdd>
+                        <BookRemove>도서 삭제</BookRemove>
                     </BookManage>
 
-                    <BookInfoList>
+                    <BookInfoListHeader>
+                        <BookCheck />
+                        <BookTitle>책 제목</BookTitle>
+                        <BookWriter>작가</BookWriter>
+                        <BookGenre>장르</BookGenre>
+                        <BookCount>권 수</BookCount>
+                        <BookDate>입고일</BookDate>
+                    </BookInfoListHeader>
 
-                    </BookInfoList>
+                    {bookList.length > 0 && bookList.map((item, index) => {
+                        return (
+                            <BookInfoList key={index}>
+                                <BookCheck />
+                                <BookTitle>{item.bookName}</BookTitle>
+                                <BookWriter>{item.bookWriter}</BookWriter>
+                                <BookGenre>{item.bookGenre}</BookGenre>
+                                <BookCount>{item.bookCnt}</BookCount>
+                                <BookDate>{item.dateReg}</BookDate>
+                            </BookInfoList>
+                        )
+                    })}
                 </BookInfoBind>
 
             </BoardBind>
@@ -43,8 +73,8 @@ const BoardBind = styled.div`
 
 const BookInfoBind = styled.div`
     width: 100%;
-    display: flex;
-    flex-direction: column;
+    height: 60vh;
+    border: 2px solid red;
 `
 
 const BookManage = styled.div`
@@ -76,8 +106,74 @@ const BookRemove = styled.div`
     margin-left: 2%;
 `
 
+const BookInfoListHeader = styled.div`
+    height: 5vh;
+    border: 2px solid blue;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`
+
 const BookInfoList = styled.div`
-    width: 100%;
-    height: 50vh;
-    border: 2px solid red;
+    height: 5vh;
+    border: 2px solid blue;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`
+
+const BookCheck = styled.div`
+    width: 5%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BookTitle = styled.div`
+    width: 25%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BookWriter = styled.div`
+    width: 25%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BookGenre = styled.div`
+    width: 15%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BookCount = styled.div`
+    width: 10%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BookDate = styled.div`
+    width: 20%;
+    height: 5vh;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
