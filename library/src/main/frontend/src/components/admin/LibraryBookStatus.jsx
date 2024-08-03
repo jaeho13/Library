497724b805a4b816107e13437d26c19f5b8322c7
jaeho13 <jaehooo13@naver.com>
2024-08-03 23:59:40 +0900

    "0803 jaeho 도서 리스트 삭제 함수"

--- library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
+++ library/src/main/frontend/src/components/admin/LibraryBookStatus.jsx
@@ -76,6 +76,16 @@ const LibraryBookStatus = () => {
         }
     };
 
+    const bookListDelete = async () => {
+        try {
+            const response = await axios.post("/book/deleteBook")
+            setBookList(response.data.bookList);
+            console.log("책 리스트 데이터 삭제 됐다");
+        } catch (error) {
+            console.log("책 리스트 데이터 삭제 안 됐다")
+        }
+    }
+
     return (
         <>
             <TopSide name="도서 관리" />
