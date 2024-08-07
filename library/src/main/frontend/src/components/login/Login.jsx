93e22a10280f4d86ee6f970342c3e07d16ab5b9e
jaeho13 <jaehooo13@naver.com>
2024-08-08 00:24:09 +0900

    "0808 jaeho 로그인 페이지 컴포넌트 스타일 분리"

--- library/src/main/frontend/src/components/login/Login.jsx
+++ library/src/main/frontend/src/components/login/Login.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import styled from "styled-components";
-// import { LoginBind, LibraryLogo, AccountCheck, InputBind, IdBind, PasswordBind, IdName, IdInput, PasswordName, PasswordInput, IdCheck } from "./styles/LoginStyle";
+import { LoginBind, LibraryLogo, AccountCheck, InputBind, IdBind, PasswordBind, IdName, IdInput, PasswordName, PasswordInput, IdCheck } from "./styles/LoginStyle";
 
 const Login = () => {
 
@@ -23,6 +22,7 @@ const Login = () => {
                 alert("아이디 비밀번호를 확인해 주세요.")
             }
         }).catch(error => {
+            alert("로그인 정보를 확인해 주세요.")
             console.log("데이터 확인 실패")
         })
     }
@@ -74,113 +74,3 @@ const Login = () => {
 }
 
 export default Login;
-
-const LoginBind = styled.div`
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /* border: 2px solid red; */
-    flex-direction: column;
-`
-
-const LibraryLogo = styled.div`
-    width: 30%;
-    max-width: 400px;
-    min-width: 400px;
-    height: 10vh;
-    min-height: 100px;
-    border: 2px solid red;
-`
-
-const AccountCheck = styled.div`
-    width: 30%;
-    max-width: 400px;
-    min-width: 400px;
-    height: 20vh;
-    min-height: 200px;
-    border: 2px solid blue;
-    margin-bottom: 10vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-`
-
-const InputBind = styled.div`
-    width: 60%;
-    height: 18vh;
-    min-height: 180px;
-    /* border: 2px solid black; */
-    display: flex;
-    flex-direction: column;
-    /* justify-content: space-evenly; */
-    justify-content: center;
-
-    align-items: center;
-`
-
-const IdBind = styled.div`
-    width: 100%;
-    height: 10vh;
-    min-height: 100px;
-    /* border: 2px solid red; */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-`
-
-const IdName = styled.div`
-    width: 100%;
-    height: 2vh;
-    min-height: 20px;
-    /* border: 2px solid black; */
-`
-
-const IdInput = styled.input`
-    width: 95%;
-    height: 4vh;
-    min-height: 40px;
-    /* border: 2px solid black; */
-    border-radius: 10px;
-`
-
-const PasswordBind = styled.div`
-    width: 100%;
-    height: 10vh;
-    min-height: 100px;
-    /* border: 2px solid red; */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-`
-
-const PasswordName = styled.div`
-    width: 100%;
-    height: 2vh;
-    min-height: 20px;
-    /* border: 2px solid black; */
-`
-
-const PasswordInput = styled.input`
-    width: 95%;
-    height: 4vh;
-    min-height: 40px;
-    border: 2px solid black;
-    border-radius: 10px;
-`
-
-const IdCheck = styled.button`
-    width: 30%;
-    max-width: 100px;
-    min-height: 100px;
-    height: 10vh;
-    min-height: 100px;
-    border: 2px solid purple;
-    font-size: 1.5rem;
-    cursor: pointer;
-`
--- library/src/main/frontend/src/components/login/styles/LoginStyle.jsx
+++ library/src/main/frontend/src/components/login/styles/LoginStyle.jsx
@@ -1,90 +1,111 @@
-// import styled from "styled-components"
+import styled from "styled-components"
 
-// export const LoginBind = styled.div`
-//     position: fixed;
-//     left: 0;
-//     top: 0;
-//     right: 0;
-//     bottom: 0;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     border: 2px solid red;
-//     flex-direction: column;
-// `
+export const LoginBind = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 2px solid red; */
+    flex-direction: column;
+`
 
-// export const LibraryLogo = styled.div`
-//     width: 30%;
-//     height: 15vh;
-//     border: 2px solid red;
-// `
+export const LibraryLogo = styled.div`
+    width: 30%;
+    max-width: 400px;
+    min-width: 400px;
+    height: 10vh;
+    min-height: 100px;
+    border: 2px solid red;
+`
 
-// export const AccountCheck = styled.div`
-//     width: 30%;
-//     height: 20vh;
-//     border: 2px solid blue;
-//     margin-bottom: 10vh;
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: space-around;
-//     align-items: center;
-// `
+export const AccountCheck = styled.div`
+    width: 30%;
+    max-width: 400px;
+    min-width: 400px;
+    height: 20vh;
+    min-height: 200px;
+    border: 2px solid blue;
+    margin-bottom: 10vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`
 
-// export const InputBind = styled.div`
-//     width: 60%;
-//     height: 18vh;
-//     /* border: 2px solid black; */
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-evenly;
-//     align-items: center;
-// `
+export const InputBind = styled.div`
+    width: 60%;
+    height: 18vh;
+    min-height: 180px;
+    /* border: 2px solid black; */
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-evenly; */
+    justify-content: center;
 
-// export const IdBind = styled.div`
-//     width: 100%;
-//     height: 10vh;
-//     /* border: 2px solid red; */
-// `
+    align-items: center;
+`
 
-// export const PasswordBind = styled.div`
-//     width: 100%;
-//     height: 10vh;
-//     /* border: 2px solid red; */
-    
-// `
+export const IdBind = styled.div`
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
 
-// export const IdName = styled.div`
-//     width: 100%;
-//     height: 2vh;
-//     /* border: 2px solid black; */
-// `
+export const IdName = styled.div`
+    width: 100%;
+    height: 2vh;
+    min-height: 20px;
+    /* border: 2px solid black; */
+`
 
-// export const IdInput = styled.input`
-//     width: 95%;
-//     height: 4vh;
-//     /* border: 2px solid black; */
-//     border-radius: 10px;
-//     margin-top: 1vh;
-// `
+export const IdInput = styled.input`
+    width: 95%;
+    height: 4vh;
+    min-height: 40px;
+    /* border: 2px solid black; */
+    border-radius: 10px;
+`
 
-// export const PasswordName = styled.div`
-//     width: 100%;
-//     height: 2vh;
-//     /* border: 2px solid black; */
-// `
+export const PasswordBind = styled.div`
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
 
-// export const PasswordInput = styled.input`
-//     width: 95%;
-//     height: 4vh;
-//     /* border: 2px solid black; */
-//     border-radius: 10px;
-//     margin-top: 1vh;
-// `
+export const PasswordName = styled.div`
+    width: 100%;
+    height: 2vh;
+    min-height: 20px;
+    /* border: 2px solid black; */
+`
 
-// export const IdCheck = styled.button`
-//     width: 30%;
-//     height: 10vh;
-//     border: 2px solid purple;
-//     font-size: 1.5rem;
-//     cursor: pointer;
-// `
+export const PasswordInput = styled.input`
+    width: 95%;
+    height: 4vh;
+    min-height: 40px;
+    border: 2px solid black;
+    border-radius: 10px;
+`
+
+export const IdCheck = styled.button`
+    width: 30%;
+    max-width: 100px;
+    min-height: 100px;
+    height: 10vh;
+    min-height: 100px;
+    border: 2px solid purple;
+    font-size: 1.5rem;
+    cursor: pointer;
+`
