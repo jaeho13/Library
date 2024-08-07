3a98bbfd13af46b765bfa9ad1a45f5f0b0a0c2c1
jaeho13 <jaehooo13@naver.com>
2024-08-08 00:21:34 +0900

    "0808 jaeho 로그인 페이지 스타일"

--- library/src/main/frontend/src/components/login/Login.jsx
+++ library/src/main/frontend/src/components/login/Login.jsx
@@ -93,6 +93,7 @@ const LibraryLogo = styled.div`
     max-width: 400px;
     min-width: 400px;
     height: 10vh;
+    min-height: 100px;
     border: 2px solid red;
 `
 
@@ -101,6 +102,7 @@ const AccountCheck = styled.div`
     max-width: 400px;
     min-width: 400px;
     height: 20vh;
+    min-height: 200px;
     border: 2px solid blue;
     margin-bottom: 10vh;
     display: flex;
@@ -112,57 +114,72 @@ const AccountCheck = styled.div`
 const InputBind = styled.div`
     width: 60%;
     height: 18vh;
+    min-height: 180px;
     /* border: 2px solid black; */
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+    /* justify-content: space-evenly; */
+    justify-content: center;
+
     align-items: center;
 `
 
 const IdBind = styled.div`
     width: 100%;
     height: 10vh;
+    min-height: 100px;
     /* border: 2px solid red; */
-`
-
-const PasswordBind = styled.div`
-    width: 100%;
-    height: 10vh;
-    /* border: 2px solid red; */
-    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
 
 const IdName = styled.div`
     width: 100%;
     height: 2vh;
+    min-height: 20px;
     /* border: 2px solid black; */
 `
 
 const IdInput = styled.input`
     width: 95%;
     height: 4vh;
+    min-height: 40px;
     /* border: 2px solid black; */
     border-radius: 10px;
-    margin-top: 1vh;
+`
+
+const PasswordBind = styled.div`
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
 
 const PasswordName = styled.div`
     width: 100%;
     height: 2vh;
+    min-height: 20px;
     /* border: 2px solid black; */
 `
 
 const PasswordInput = styled.input`
     width: 95%;
     height: 4vh;
-    /* border: 2px solid black; */
+    min-height: 40px;
+    border: 2px solid black;
     border-radius: 10px;
-    margin-top: 1vh;
 `
 
 const IdCheck = styled.button`
     width: 30%;
+    max-width: 100px;
+    min-height: 100px;
     height: 10vh;
+    min-height: 100px;
     border: 2px solid purple;
     font-size: 1.5rem;
     cursor: pointer;
